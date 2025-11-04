fb83d6df27328ddd77c01976910d11dd44a20ae1
foxler2010 <drew@drewssite.top>
2025-11-04 11:59:56 -0600

    really close to getting signal_alarm program to work

--- signal_alarm.c
+++ signal_alarm.c
@@ -6,7 +6,33 @@
  * Compile with: make signal_alarm
  */
 
+#define _POSIX_C_SOURCE 200809L
+#define ALARM_DURATION 5
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "signal_alarm.h"
+
 int main(int argc, char *argv[])
 {
-    return 0;
+    struct sigaction *act = calloc(1, sizeof(struct sigaction));
+    act->sa_flags = SA_SIGINFO;
+    // TODO sa_handler || sa_sigaction = my signal_handler() function
+    sigaction(SIGALRM, act, NULL);
+    alarm(ALARM_DURATION);
+    int i = 0;
+    while(1) {
+        sleep(1);
+        i++;
+        printf("Waited for %ds...", i);
+    }
+    return EXIT_FAILURE;
+}
+
+int signal_handler()
+{
+    printf("Alarm signal received!");
+    exit(EXIT_SUCCESS);
 }
---
+++ signal_alarm.h
@@ -0,0 +1,9 @@
+/*
+ * Filename: signal_alarm.h
+ * Description: Header file for signal_alarm.c
+ * Author: Drew Malone <malonea@msoe.edu>
+ * Created: 11/4/25
+ * Compile with: make signal_alarm
+ */
+
+int signal_handler();
