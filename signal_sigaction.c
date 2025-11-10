9bff71879e44d81458b731245e6b3e2748d7059f
foxler2010 <drew@drewssite.top>
2025-11-10 10:47:15 -0600

    initial implementation of signal_sigaction program; does not get advanced info from the signal yet

--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,34 @@
-/**
- * File: 
- * Modified by:
- * 
- * Brief summary of program:
+/*
+ * Filename: signal_sigaction.c
+ * Description: Demo of sigaction() system call,
+ *              and getting info from signals
+ * Author: Drew Malone <malonea@msoe.edu>
+ * Created: 11/10/25
+ * Compile with: make signal_sigaction
  */
 
+#include "signal_sigaction.h"
 
+int main(int argc, char *argv[])
+{
+    struct sigaction act = {0};
+    act.sa_handler = sigusr1_handler;
+    // TODO sa_flags
+    sigaction(SIGUSR1, &act, NULL);
+
+    while (1) {
+        // wait for SIGUSR1 signal
+        // it can be sent with the command:
+        //     kill -SIGUSR1 pid
+        // where "pid" is the PID of this program
+    }
+
+    return EXIT_FAILURE;
+}
+
+void sigusr1_handler(int signum)
+{
+    printf("SIGUSR1 recieved!\n");
+    exit(EXIT_SUCCESS);
+    // TODO print PID of signal's sender
+}
---
+++ signal_sigaction.h
@@ -0,0 +1,15 @@
+/*
+ * Filename: signal_sigaction.h
+ * Description: Header file for signal_sigaction.c
+ * Author: Drew Malone <malonea@msoe.edu>
+ * Created: 11/10/25
+ * Compile with: make signal_sigaction
+ */
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+void sigusr1_handler(int signum);
